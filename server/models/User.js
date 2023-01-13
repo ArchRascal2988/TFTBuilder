9cda1a7ee7c877b4620304819be083f5ca3f58c8
Lex Matthews <tanukiguernica@gmail.com>
2023-01-13 15:01:51 -0500

    made user model done for today

---
+++ server/models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model }= require('mongoose');
+const { hash, compare }= require('bcrypt');
+
+const UserSc= new Schema(
+    {
+            username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 8,
+            unique: true
+        }
+    }    
+);
+    
+UserSc.pre("save", function(next){
+    let user= this;
+
+    hash(user.password, 10, (err, res)=>{
+        if(err) return next(err);
+
+        user.password= res;
+        next();
+    })
+})
+
+UserSc.methods.checkPw = async function(pw) {
+    return compare(pw, this.password)
+};
+
+const User= model("User", UserSc);
